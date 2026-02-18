6ed86fb4f8351cdaa7c912a6d7ce7619572e9fb3
AuanJulio <auanjulio13@gmail.com>
2026-02-18 17:16:29 -0300

    first commit

---
+++ src/main/java/com/portflow/PortFlowApiApplication.java
@@ -0,0 +1,13 @@
+package com.portflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortFlowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortFlowApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portflow/PortFlowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortFlowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
