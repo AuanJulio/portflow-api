4d1ffc0ff89a32984921e784c7ce8faeb301dce1
AuanJulio <auanjulio13@gmail.com>
2026-02-20 18:28:06 -0300

    feat(gateway): create Container gateway interface and infra adapter

---
+++ src/main/java/com/portflow/core/gateway/ContainerGateway.java
@@ -0,0 +1,4 @@
+package com.portflow.core.gateway;
+
+public interface ContainerGateway {
+}
---
+++ src/main/java/com/portflow/infra/gateway/ContainerRepositoryGateway.java
@@ -0,0 +1,6 @@
+package com.portflow.infra.gateway;
+
+import com.portflow.core.gateway.ContainerGateway;
+
+public class ContainerRepositoryGateway implements ContainerGateway {
+}
